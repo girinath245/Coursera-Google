e31716f25f3eaf57dd6fd0e367beb6a05a10f019
Girinath <girinath2452001@gmail.com>
2022-02-15 04:04:45 +0530

    number 2

--- example.py
+++ example.py
@@ -1,5 +1,6 @@
 def git_opertion():
- print("I am updating example.py file to the remote repository.")
+    print("I am updating example.py file to the remote repository.")
+    print("Yet another line")
 
 if __name__ == "__main__" : 
     git_opertion()
